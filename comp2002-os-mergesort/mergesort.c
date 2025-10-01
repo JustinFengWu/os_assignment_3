bb487c1ecf6b4b2cf8d55deef4adced825f11eb6
JustinFWu <166305674+JustinFWu@users.noreply.github.com>
2025-10-01 23:21:05 +0930

    just completed merge and struct arg function

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -9,6 +9,40 @@
 
 /* this function will be called by mergesort() and also by parallel_mergesort(). */
 void merge(int leftstart, int leftend, int rightstart, int rightend){
+	int leftStart = leftstart; int leftStartB = leftStart;
+	int rightStart = rightstart;
+
+	// merge the two arrays by putting digits into B temporarily
+	while (leftStart <= leftend && rightStart <= rightend) {
+		if (A[leftStart] <= A[rightStart]) {
+			B[leftStartB] = A[leftStart];
+			leftStart++;
+			leftStartB++;
+		} else if(A[leftStart] > A[rightStart]) {
+			B[leftStartB] = A[rightStart];
+			leftStartB++;
+			rightStart++;
+		}
+	}
+
+	// order of these two cleanups dont matter
+	while (leftStart <= leftend) {
+		B[leftStartB] = A[leftStart];
+			leftStart++;
+			leftStartB++;
+	}
+	while (rightStart <= rightend) {
+		B[leftStartB] = A[rightStart];
+		leftStartB++;
+		rightStart++;
+	}
+
+	// Copy back into A
+	for (int i = leftstart; i <= rightend; i++) {
+		A[i] = B[i];
+	}
+
+	return;
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
@@ -22,6 +56,10 @@ void * parallel_mergesort(void *arg){
 
 /* we build the argument for the parallel_mergesort function. */
 struct argument * buildArgs(int left, int right, int level){
-		return NULL;
+	struct argument * arg = malloc(sizeof(struct argument));
+	arg->left = left;
+	arg->right = right;
+	arg->level = level;	
+	return arg;
 }
 
