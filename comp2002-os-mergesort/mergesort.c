b8afa3e2e0fe18282ca5bfdfba0d9d9eba9b8860
JustinFWu <166305674+JustinFWu@users.noreply.github.com>
2025-10-02 22:51:05 +0930

    completed parallel

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -47,11 +47,49 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
+	if (left == right) {
+		return;
+	}
+
+	int mid = (left + right) / 2;
+	my_mergesort(left, mid);
+	my_mergesort(mid + 1, right);
+	merge(left, mid, mid + 1, right);
 }
 
 /* this function will be called by the testing program. */
 void * parallel_mergesort(void *arg){
-		return NULL;
+	struct argument *args = arg;
+    int left = args->left;
+    int right = args->right;
+    int level = args->level;
+	int mid = (left + right) / 2;
+
+	// reaching base case
+    if (left == right) {
+        free(args);
+        return;
+    }
+
+    if (level < cutoff) {
+        pthread_t threadId1, threadId2;
+        struct argument *leftArgs = buildArgs(left, mid, level + 1);
+        struct argument *rightArgs = buildArgs(mid + 1, right, level + 1);
+
+        pthread_create(&threadId1, NULL, parallel_mergesort, leftArgs);
+        pthread_create(&threadId2, NULL, parallel_mergesort, rightArgs);
+
+        pthread_join(threadId1, NULL);
+        pthread_join(threadId2, NULL);
+    } else {
+        // once cutoff has been reached use recursive version
+        my_mergesort(left, mid);
+        my_mergesort(mid + 1, right);
+    }
+    merge(left, mid, mid + 1, right);
+
+    free(args);
+    return;
 }
 
 /* we build the argument for the parallel_mergesort function. */
